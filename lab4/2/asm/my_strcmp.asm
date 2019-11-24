627a624134d9618be8f259caa265326e03815994
Tom Ronen <mystic74@gmail.com>
2019-11-24 16:23:40 +0200

    lab4 : no more segv in 2

--- lab4/2/asm/my_strcmp.asm
+++ lab4/2/asm/my_strcmp.asm
@@ -1,32 +1,38 @@
 section .text
 	global cmpstr ;make strcmp transparent to other modules
-strcmp:
+cmpstr:
 	push ebp ;stack maintenance
 	mov ebp, esp ;stack maintenance
 	push ecx;
 	push ebx;
+	push edx;
 	
 get_arguments:
-	movzx eax, BYTE [ebp+8] ;retrieves the first function argument, READ about MOVZX
-	movzx ecx, BYTE [ebp+12] ;retrieves the second function argument, READ about MOVZX
+	mov eax, [ebp+8] ;retrieves the first function argument, should be string pointer
+	mov ebx, [ebp+12] ;retrieves the second function argument, should be string pointer
 
-cmpstr:
+my_comp:
 	mov ecx, 0; Loop index
 str_cmp_loop:
-	 mov dl, BYTE [eax+ecx]
-     mov dh, BYTE [ebx+ecx]
+	 mov  dl,  byte [eax + ecx] ; taking the value at pointer eax + value of ecx
+     mov  dh,  byte [ebx + ecx] ; taking the value at pointer ebx + value of ecx
      inc ecx
-     cmp dl, 0 ; Check null term
-     cmp BYTE dl, BYTE dh
+     cmp al, 0
+     je F_FINISHED
+     cmp dh, dl  ; Check null term
      je str_cmp_loop
      jl F_BIG
      jg S_BIG
+     
 
+     ;cmp  dl, dh
+     
 F_FINISHED:
 	cmp dh, 0
 	jne S_BIG
 	mov eax, 0;
 	jmp FINISH
+
 F_BIG:
 	mov eax, 1 ;return value need to be stored in eax register
 	jmp FINISH
@@ -36,6 +42,7 @@ S_BIG:
 	jmp FINISH
 	
 FINISH:
+	pop edx;
 	pop ebx;
 	pop ecx;
 	mov esp, ebp ;stack maintenance
