b22c119bef4c1a84af12fcbfe8ebcebfde9ffb69
Tom Ronen <mystic74@gmail.com>
2019-12-01 10:41:59 +0200

    task2a : now looping on entire file
    Till now was just printing once,
    now printing the whole shit.

--- lab5/2/2a.asm
+++ lab5/2/2a.asm
@@ -3,7 +3,7 @@ section .data
 section .bss
   descriptor resb 4 ;memory for storing descriptor
   buffer resb 1025
-  len equ 1024
+  len equ 50
 section .text
          global main
 
@@ -15,12 +15,23 @@ main:
 
   mov [descriptor],eax ;storing the descriptor
 
+READ_ME:
   mov eax,3 ;read from file
   mov ebx,[descriptor] ;your file descriptor
   mov ecx,buffer ;read to buffer
   mov edx,len ;read len bytes
   int 80h ;read len bytes to buffer from file
 
+  cmp eax, len
+  jne FINISH
+  mov edx,eax ;storing count of readed bytes to edx
+  mov eax,4 ;write to file
+  mov ebx,1 ; stdout
+  mov ecx,buffer ;from buffer
+  int 80h ;write to terminal all readed bytes from buffer
+  jmp READ_ME
+  
+FINISH:
   mov edx,eax ;storing count of readed bytes to edx
   mov eax,4 ;write to file
   mov ebx,1 ; stdout
